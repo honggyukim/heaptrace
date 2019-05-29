d35949370dc249121f9046e27380aaf04af9c8c8
Honggyu Kim <hong.gyu.kim@lge.com>
2019-05-29 14:46:26 +0900

    utils: Introduce fmt_string class
    fmt_string class is a convinient wrapper of std::string.  It supports
    asprintf style string composition as well.
    
    The following usage can be accepted.
    
      fmt_string s("five: %d, pi: %lf", 5, 3.14);
    
    Signed-off-by: Honggyu Kim <hong.gyu.kim@lge.com>

--- utils.h
+++ utils.h
@@ -4,6 +4,9 @@
 #define HEAPTRACE_UTILS_H
 
 #include <cstdio>
+#include <cstdarg>
+
+#include <string>
 
 class file_t {
 public:
@@ -18,6 +21,65 @@ public:
 
 private:
 	FILE *fp;
+};
+
+class fmt_string {
+public:
+	fmt_string()  { }
+	~fmt_string() { }
+	fmt_string(const fmt_string& s) : str(s.str) { }
+	fmt_string(const std::string& s) : str(s)  { }
+#define FMT_IMPL					\
+	do {						\
+		va_list args;				\
+		char *ptr;				\
+		int ret;				\
+		va_start(args, fmt);			\
+		ret = vasprintf(&ptr, fmt, args);	\
+		this->str = ptr;			\
+		free(ptr);				\
+		va_end(args);				\
+	} while (0)
+
+	fmt_string(const char* fmt, ...) {
+		FMT_IMPL;
+	}
+
+	explicit fmt_string(size_t size) {
+		this->str.resize(size);
+	}
+
+	void fmtset(const char* fmt, ...) {
+		FMT_IMPL;
+	}
+	const char* get() const {
+		return this->str.c_str();
+	}
+	char* data() {
+		return const_cast<char*>(this->str.data());
+	}
+
+	fmt_string operator+(const fmt_string& s) const {
+		return fmt_string(this->str + s.str);
+	}
+	fmt_string& operator=(const fmt_string& s) {
+		this->str = s.str;
+		return *this;
+	}
+	bool operator==(const fmt_string& s) const {
+		return this->str == s.str;
+	}
+
+	// conversion operators
+	operator const char*() const {
+		return str.c_str();
+	}
+	operator const std::string() const {
+		return str;
+	}
+
+private:
+	std::string str;
 };
 
 #endif /* HEAPTRACE_UTILS_H */
