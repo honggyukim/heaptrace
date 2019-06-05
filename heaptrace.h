8008664b7e2a12263bc380758e115eaebe3cb978
Honggyu Kim <hong.gyu.kim@lge.com>
2019-06-05 09:09:48 +0900

    heaptrace: Add pr_red and color scheme
    Signed-off-by: Honggyu Kim <hong.gyu.kim@lge.com>

--- heaptrace.h
+++ heaptrace.h
@@ -3,6 +3,17 @@
 #ifndef HEAPTRACE_HEAPTRACE_H
 #define HEAPTRACE_HEAPTRACE_H
 
+#define TERM_COLOR_NORMAL   ""
+#define TERM_COLOR_RESET    "\033[0m"
+#define TERM_COLOR_BOLD     "\033[1m"
+#define TERM_COLOR_RED      "\033[91m"    /* bright red */
+#define TERM_COLOR_GREEN    "\033[32m"
+#define TERM_COLOR_YELLOW   "\033[33m"
+#define TERM_COLOR_BLUE     "\033[94m"    /* bright blue */
+#define TERM_COLOR_MAGENTA  "\033[35m"
+#define TERM_COLOR_CYAN     "\033[36m"
+#define TERM_COLOR_GRAY     "\033[90m"    /* bright black */
+
 #ifdef DEBUG
 #define pr_dbg(fmt, ...) fprintf(stderr, fmt, ## __VA_ARGS__)
 #else
@@ -10,6 +21,8 @@
 #endif
 
 #define pr_out(fmt, ...) fprintf(stderr, fmt, ## __VA_ARGS__)
+
+#define pr_red(fmt, ...) fprintf(stderr, TERM_COLOR_RED fmt TERM_COLOR_RESET, ## __VA_ARGS__)
 
 struct thread_flags_t {
 	// to protect unexpected recursive malloc calls
