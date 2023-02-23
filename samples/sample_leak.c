475f0547375603a8d065404f24791fe720ec2679
Bojun Seo <bojun.seo@lge.com>
2023-02-23 14:59:03 +0900

    samples: Add simple sample which generate memory leak
    heaptrace report nothing on samples/sample.c test program
    Add simple test program to check whether it detect memory allocation normally
    
    Signed-off-by: Bojun Seo <bojun.seo@lge.com>

---
+++ samples/sample_leak.c
@@ -0,0 +1,18 @@
+/* Copyright (c) 2022 LG Electronics Inc. */
+/* SPDX-License-Identifier: GPL-2.0 */
+#include <stdlib.h>
+
+int main(void)
+{
+	int *p;
+
+	p = (int*)malloc(4);
+	//free(p);
+
+	p = (int*)calloc(4, 10);
+
+	p = (int*)realloc(p, 1000000);
+	free(p);
+
+	return 0;
+}
